89f7774c6ca2e29981bc9fadd7e6d56eedd44401
Adrianna Sarzynska <ada@MacBook-Air-Adrianna.local>
2017-09-16 21:16:10 +0200

    creating the third component

---
+++ app/component2/component3.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-component3',
+  template: `
+    <p>
+      component3 Works!
+    </p>
+  `,
+  styles: []
+})
+export class Component3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
